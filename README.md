369044298e6f0a981fcd6954c49e2da22ad058e0
okivictor <102834999+okivictor@users.noreply.github.com>
2022-06-23 16:31:18 +0200

    An introduction to JS (Simple algorithms)

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # M2-W5-D1
- 
+Simple algorithms ---> An introduction to JS.
---
+++ index.js
@@ -0,0 +1,20 @@
+let spaghetti = 400;
+let guanciale = 250;
+let eggYolks = 6;
+let pecorinoRomanoCheese = 50;
+let blackPepper = 4;
+
+let eggYolkAndCheese = eggYolks + pecorinoRomanoCheese;
+
+let roastedBlackpepper = blackPepper + eggYolkAndCheese;
+
+let pastaMixture = guanciale + eggYolkAndCheese;
+
+let carbonara = pastaMixture + roastedBlackpepper;
+
+console.log(carbonara);
+
+
+
+
+
